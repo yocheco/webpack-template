620cf56ee0a72be43e1bf1a5085aa050613a5889
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-09-27 14:04:50 -0500

    :construction: -Add workflow

---
+++ lighthouserc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  ci: {
+    assert: {
+      preset: "lighthouse:no-pwa",
+      assertions: {
+        // Performance related
+        "font-display": "warn",
+        "unsized-images": "warn",
+        "unused-css-rules": "warn",
+        "unused-javascript": "warn",
+        "uses-optimized-images": "warn",
+        "uses-responsive-images": "warn",
+        "unminified-css": "warn",
+        "third-party-facades": "warn",
+        "offscreen-images": "warn",
+        // Not performance related
+        "tap-targets": "off",
+        "non-composited-animations": "off",
+        "button-name": "off",
+        "color-contrast": "off",
+        "csp-xss": "off",
+        "errors-in-console": "off",
+        "external-anchors-use-rel-noopener": "off",
+        "heading-order": "off",
+        "link-name": "off",
+        "meta-description": "off",
+        "uses-text-compression": "off",
+      },
+    },
+  },
+};
