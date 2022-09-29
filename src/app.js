7d8bc5b50669bff2271eb814e3bc294fb449f5e1
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-09-28 22:19:29 -0500

    :construction: -Add Prefetch

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import "./js/theme/index"
 import "./sass/main.scss"
+// import { playerYoutubeStart } from "./js/player-youtube"
 
-import { playerStart } from "./js/player"
-import { playerYoutubeStart } from "./js/player-youtube"
 
-// play and load videos
-playerStart()
-// play and load videos youtube
-playerYoutubeStart()
+document.onload = init();
+
+function init() {
+  // play and load videos
+  import(/* webpackPrefetch: true */ "./js/player").then(({ playerStart }) => {
+    playerStart()
+  });
+  // play and load videos youtube
+  import(/* webpackPrefetch: true */ "./js/player-youtube").then(({ playerYoutubeStart }) => {
+    playerYoutubeStart()
+  });
+}
